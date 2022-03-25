c7d5a465a2b7c3cb1a7e7025c9ed939f15459aaa
addaesg <casg@cin.ufpe.br>
2022-03-25 09:08:18 -0300

    Uploading Atom Structures: Point, RGB, VectorOrigin, LineOrigin

--- misc/ga/shapes.cpp
+++ misc/ga/shapes.cpp
@@ -18,13 +18,26 @@ Sphere::Radius::Radius() = default;
 Sphere::Radius::Radius(double radius): r(radius){};
 
 Sphere::Equation::Equation() = default;
-Sphere::Equation::Equation(atom::Point center, Sphere::Radius radius): center(center), radius(radius)
-{
-    values = {center, radius};
-};
+Sphere::Equation::Equation(atom::Point center, Sphere::Radius radius): center(center), radius(radius){};
+
 
 Sphere::Sphere() = default;
-Sphere:::Sphere(Sphere::Equation formula, atom::Rgb color)
+Sphere::Sphere(Sphere::Equation formula, atom::Rgb color)
 {
-    
+    this->equation = formula;
+    this->color = color;
+}
+
+
+/* 
+    Plane
+*/
+Plane::Equation::Equation() = default;
+Plane::Equation::Equation(atom::Point p, atom::VectorOrigin n): point(p), normal(n){};
+
+Plane::Plane() = default;
+Plane::Plane(Plane::Equation formula, atom::Rgb color)
+{
+    this->equation = formula;
+    this->color = color;
 }
--- misc/ga/shapes.h
+++ misc/ga/shapes.h
@@ -7,42 +7,74 @@
 
 namespace shapes
 {
+    // Classe Solid: Classe pai para Esfera e Plano
     class Solid
     {
     public:
+        // cor do sólido
         atom::Rgb color;
         Solid();
         ~Solid();
     }; 
 
+    // Classe Sphere(Equation, color): Basta ler o nome
+    // Atributos:
+    //   Equation = Sphere::Equation
+    //   Color = atom::Rgb
     class Sphere: public Solid
     {
     public:
-        struct Radius{double r = 0.0; Radius(double radius); Radius();};
-        struct Equation{ 
-            atom::Point center;  Sphere::Radius radius;
-            std::pair<atom::Point, Sphere::Radius> values = {center, radius}; 
-            Equation(atom::Point, Sphere::Radius); Equation();
-
+        /*
+          Representar o Raio de uma Esfera
+          Struct Sphere::Radius = {double radius} 
+         */
+        struct Radius{
+            double r = 0.0; 
+            Radius(double radius); 
+            Radius();
         };
-
+        /*
+            Representar os elementos da equação de uma Esfera
+            Struct Sphere::Equation = {atom::Point center, Sphere::Radius r}
+        */ 
+        struct Equation{ 
+            atom::Point center;  
+            Sphere::Radius radius;
+            Equation(atom::Point, Sphere::Radius); 
+            Equation();
+        };
+        // equação da esfera
         Sphere::Equation equation;
-        Sphere::Radius rad;
-        atom::Point center;
         Sphere();
         Sphere(Sphere::Equation, atom::Rgb);
     };
 
+
+    /* 
+    Classe Plane(Equation, color): Basta ler
+    Atributos:
+        Equation = Plane::Equation
+        Color = atom::Rgb 
+    */
     class Plane: public Solid
     {
     public:
+        /*
+            Representar os elementos da equação de um Plano
+            Struct Plane::Equation = {atom::Point PontoDoPlano, atom::VectorOrigin VetorNormal}
+        */ 
         struct Equation{ 
             atom::Point point; 
             atom::VectorOrigin normal;
-            std::pair<atom::Point, atom::VectorOrigin> values = {point, normal}; 
-            Equation(atom::Point, atom::VectorOrigin); Equation();
-        }
-    }
+            Equation(atom::Point, atom::VectorOrigin);
+            Equation();  
+        };
+        // equação do plano
+        Plane::Equation equation;
+        Plane();
+        Plane(Plane::Equation, atom::Rgb);
+
+    };
 
 }
 
